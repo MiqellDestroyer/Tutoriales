a6f7a75b7cfc2ff388f9e6b91512ede05db679ed
Hunvee3 <hunvee3@gmail.com>
2017-03-29 11:56:08 -0300

    Primer Commit! Proyecto: HealthBar Estado: Completo Commit: Se agrego el proyecto completo bajo la carpeta Assets/HealthBar

---
+++ Assets/HealthBar/Scripts/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace HealthBar
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField]
+        private float health;
+        [SerializeField]
+        private  float maxHealth;
+        [SerializeField]
+        private Image healthImage;
+
+
+
+        private void Start()
+        {
+            health = maxHealth;
+        }
+
+        [ContextMenu("AddHealth")]
+        public void AddHealth(float amount)
+        {
+            health += amount;
+            if (health > maxHealth)
+            {
+                health = maxHealth;
+            }
+            else if (health < 0)
+            {
+                health = 0;
+                //muerto, deberiamos soltar un evento para todos los subscriptos.
+            }
+            UpdateHealthUI();
+        }
+
+        private void UpdateHealthUI()
+        {
+            healthImage.fillAmount = ((1) / (maxHealth)) * health;
+        }
+
+    }
+}
