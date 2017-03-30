1e8ad090a035902e4a33bd02ff42c707e01042a4
Hunvee3 <hunvee3@gmail.com>
2017-03-29 23:04:05 -0300

    Proyecto: ClickToMove Estado: Completo Commit: Se agrego el proyecto completo bajo la carpeta Assets/ClickToMove, incluye un modelo con animaciones y 2 materiales basicos de superficie. Se agrego la capa Ground.

---
+++ Assets/ClickToMove/Scripts/ClickToMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace ClickToMove
+{
+    public class ClickToMove : MonoBehaviour
+    {
+        private NavMeshAgent navAgent;
+        private Animator anim;
+
+        public Vector3 targetPos;
+        public LayerMask groundLayer; // capa del suelo;
+
+        private void Awake()
+        {
+            navAgent = GetComponent<NavMeshAgent>();
+            anim = GetComponentInChildren<Animator>();
+        }
+
+        private void Update()
+        {
+            anim.SetFloat("velocity", navAgent.velocity.sqrMagnitude);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                MoveTowardsClick();
+            }
+        }
+
+        private void MoveTowardsClick()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+            {
+                if (targetPos != hit.point)
+                {
+                    targetPos = hit.point;
+                }
+
+                navAgent.SetDestination(targetPos);
+            }
+        }
+    }
+}
